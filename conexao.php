8698e380983109ea89a9c82ec18c29d1adfa0543
LukinMar <48739554+LukinMar@users.noreply.github.com>
2019-04-15 14:31:28 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 try {
-    $host = 'localhost';
-    $dbname = 'sistemairrigacao';
-    $user = 'root';
-    $pass = '';
+    $host = 'ec2-50-19-127-115.compute-1.amazonaws.com';
+    $dbname = 'de8msggjb5bkg';
+    $user = 'btitzqmyqfdjfl';
+    $pass = '1b76f41706bfe03e2c7a367748d50dfa30513a4583cdd5e29a794a4c5f76a04a';
     
     $pdo = new PDO('mysql:host=' . $host . ';dbname='. $dbname . ';charset=utf8', $user, $pass);
 } catch (PDOException $erro) {
