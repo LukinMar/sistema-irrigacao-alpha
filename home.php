6cfdffa1b004a96fc3907db0153feaf62b1f10da
LukinMar <48739554+LukinMar@users.noreply.github.com>
2019-04-19 18:32:38 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -9,28 +9,38 @@
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
     <link rel="manifest" href="images/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#00aba9">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="msapplication-TileColor" content="#00aba9" name="theme-color" content="#ffffff">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="assets/css/home.css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Sistema Irrigação</title>
 </head>
 <style>
-    body{
+    
+        body{
          background-image: url(images/home.png);
-	 margin:0px;
          background-position:center;
-         background-color:#2bbbad;
-         background-attachment:fixed;
-         background-repeat: no-repeat;
-         background-size: cover;
-         -moz-background-size: 100% 100%;
-    -webkit-background-size: 100% 100%;
-    background-size: auto, auto;
-    }                        
+         background-color:#26a69a;
+         -webkit-border-background-size: cover;
+         -moz-border-background-size: cover;
+         -ms-border-background-size:cover;
+         -o-border-background-size:cover;
+         background-size:cover;
+        }
+        
+        @media only screen and (max-device-width: 1100px) {
+        body {
+         background-image: url(images/homemobile.png);
+         background-position:center;
+         background-color:#26a69a;
+         -webkit-border-background-size: cover;
+         -moz-border-background-size: cover;
+         -ms-border-background-size:cover;
+         -o-border-background-size:cover;
+         background-size:cover;
+         }
+
 </style>
-<body>
+<body class="img-responsive">
         <div class="navbar-fixed">
           <div class="nav-wrapper"></div>
 
