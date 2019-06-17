7a130a3e027c85b24e00ef1604fb6ffbaa58fe11
Lucas Martins <lucasb189@gmail.com>
2019-06-16 23:43:35 -0300

    android app

---
+++ android/SistemaDeIrrigacao/app/src/main/java/com/example/sistemadeirrigacao/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sistemadeirrigacao;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.DownloadListener;
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webviewid);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.setDownloadListener(new DownloadListener() {
+
+            @Override
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimetype,
+                                        long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(
+                        Uri.parse(url));
+
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Relatório-Leituras.pdf");
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Baixando Arquivo...", //To notify the Client that the file is being downloaded
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+        webView.loadUrl("https://sistema-irrigacao-v1.000webhostapp.com/login.php");
+    }
+}
+
