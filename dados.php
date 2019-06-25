6450c7674f303acb80fe854f044ac77fb2d336b6
Lucas Martins <lucasb189@gmail.com>
2019-06-25 14:40:41 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -13,12 +13,21 @@
     <meta name="theme-color" content="#ffffff">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://kit.fontawesome.com/e97cfc6afc.js"></script>
     <link rel="stylesheet" href="assets/css/dados.css"/>
     <meta charset="utf-8">
     <title>Banco de Dados</title>
     </head>
     <body class="body">
+    <div class="preloader">
+        <div class="sk-folding-cube">
+            <div class="sk-cube1 sk-cube"></div>
+            <div class="sk-cube2 sk-cube"></div>
+            <div class="sk-cube4 sk-cube"></div>
+            <div class="sk-cube3 sk-cube"></div>
+        </div>
+    </div>
         <div class="container"> 
            <div class="form">
             <form action="" method="POST">
@@ -80,5 +89,16 @@
         ?>
     </div>
     </div>
+
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/contact-form.js"></script>
+    <script src="assets/js/jquery.parallax-1.1.3.js"></script>
+    <script src="assets/js/scrollUp.min.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
     </body>
 </html>
